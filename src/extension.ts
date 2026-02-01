49473896392338b1371c96c7ec5ead229cd98ec8
Caspar5911 <Caspar95galaxy@gmail.com>
2026-02-01 12:54:15 +0800

    feat: add minimal Forge VS Code extension shell

---
+++ src/extension.ts
@@ -0,0 +1,11 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext): void {
+  const command = vscode.commands.registerCommand('forge.run', () => {
+    void vscode.window.showInformationMessage('Forge is alive');
+  });
+
+  context.subscriptions.push(command);
+}
+
+export function deactivate(): void {}
